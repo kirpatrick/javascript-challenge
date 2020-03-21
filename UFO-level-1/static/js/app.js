99104ea400aec1650404ef3ba74b09f6643e6d85
KP Dorsey <dorsey_k17@hotmail.com>
2020-03-21 08:18:54 -0500

    Data displayed on myIndex.html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,7 +2,7 @@
 var tableData = data;
 
 // Test connectivity
-console.log("Hello Alien World!");
+// console.log("Hello Alien World!");
 
 // Test connection to tableData Object
 // forEach 'sighting' in the tableData Object...
@@ -20,3 +20,27 @@ tableData.forEach((sighting) => {
     
   }); // end tableData forEach
 
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
+
+// Use D3 to select the table
+var table = d3.select("table");
+
+// Use D3 to set the table class to `table table-striped`
+table.attr("class", "table table-striped");
+
+// forEach 'sighting' in the tableData Object...
+tableData.forEach((sighting) => {
+
+    // Append one table row per sighting
+    var row = tbody.append("tr");
+
+    //forEach entry in the tableData Object...
+    Object.entries(sighting).forEach(
+        // Use the arrow function to grab each key and value...
+        ([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+    });
+    
+  }); // end tableData forEach
