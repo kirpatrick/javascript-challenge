ae79a8c2d4fd42279d6f9ed53c858f71861c8752
KP Dorsey <dorsey_k17@hotmail.com>
2020-03-21 08:50:11 -0500

    Working field entry and button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,19 +6,43 @@ var tableData = data;
 
 // Test connection to tableData Object
 // forEach 'sighting' in the tableData Object...
-tableData.forEach((sighting) => {
-    // display individual sighting info on the console
-    // console.log(sighting);
+// tableData.forEach((sighting) => {
+//     // display individual sighting info on the console
+//     // console.log(sighting);
   
-    //forEach entry in the tableData Object...
-    Object.entries(sighting).forEach(
-        // Use the arrow function to grab each key and value...
-        ([key, value]) => {
-      // and write each key and value to the console.
-      console.log(`Key: ${key} and Value ${value}`);
-    });
+//     //forEach entry in the tableData Object...
+//     Object.entries(sighting).forEach(
+//         // Use the arrow function to grab each key and value...
+//         ([key, value]) => {
+//       // and write each key and value to the console.
+//       console.log(`Key: ${key} and Value ${value}`);
+//     });
     
-  }); // end tableData forEach
+//   }); // end tableData forEach
+
+/****************************************************************/
+// Create actions for form and filter
+// Select the button
+var button = d3.select("#button");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#sighting-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  console.log(filteredData);
+});
+/****************************************************************/
+
+
+
 
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
@@ -39,8 +63,9 @@ tableData.forEach((sighting) => {
     Object.entries(sighting).forEach(
         // Use the arrow function to grab each key and value...
         ([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
+            // Append sightingDetail to the html table row
+            var sightingDetail = row.append("td");
+            sightingDetail.text(value);
     });
     
   }); // end tableData forEach
